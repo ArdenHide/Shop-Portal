b8bd8d8864644dedab241f987c6e6f02618c9cd7
Стас Высоцкий <ardenhide@gmail.com>
2020-11-10 10:43:18 +0300

    start commit

---
+++ Models/Generes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPortal.Models
+{
+    public class Generes
+    {
+        public int id { get; set; }
+
+        public string Name { get; set; }
+
+        public int countSong { get; set; }
+
+        public bool isFavorite { get; set; }
+
+        public List<Song> Songs { get; set; }
+    }
+}
---
+++ Models/Song.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPortal.Models
+{
+    public class Song
+    {
+        public int id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ArtistName { get; set; }
+        
+        public string Published { get; set; }
+
+        public string countPlay { get; set; }
+
+        public string Description { get; set; }
+
+        public bool isFavorite { get; set; }
+
+        public List<Song> Songs { get; set; }
+    }
+}
